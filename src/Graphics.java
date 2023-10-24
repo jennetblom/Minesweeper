7a18cd66e231cdda3fcf411347624ac532607710
Jennet Blom <jeboblom20@hotmail.com>
2023-10-24 09:56:50 +0200

    Created new project

---
+++ src/Graphics.java
@@ -0,0 +1,73 @@
+public class Graphics {
+//
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("|   |   |   |   |   |   |   |   |   |   |   |   |");
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("|   |   |   |   |   |   |   |   |   |   |   |   |");
+//        System.out.println("-------------------------------------------------");
+//        System.out.println("|   |   |   |   |   |   |   |   |   |   |   |   |");
+//        System.out.println("-------------------------------------------------");
+//
+//
+//
+//
+//        System.out.println("___________________________________________");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|_____|_____|_____|_____|_____|_____|_____|");
+//
+//
+//
+//        System.out.println("+-----+-----+-----+-----+-----+-----+-----+");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("+-----+-----+-----+-----+-----+-----+-----+");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|  x  |  0  |     |     |     |     |     |");
+//        System.out.println("+-----+-----+-----+-----+-----+-----+-----+");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("+-----+-----+-----+-----+-----+-----+-----+");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("+-----+-----+-----+-----+-----+-----+-----+");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("+-----+-----+-----+-----+-----+-----+-----+");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("+-----+-----+-----+-----+-----+-----+-----+");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("|     |     |     |     |     |     |     |");
+//        System.out.println("+-----+-----+-----+-----+-----+-----+-----+");
+//
+
+
+
+
+
+
+
+
+}
